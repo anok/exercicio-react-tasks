9b9f56ede8c5540b16393a5bf560af7a83d89f42
Rodrigo Lopes <rodrigolaquino@gmail.com>
2020-04-26 16:14:40 -0400

    Primeiro commit

---
+++ src/screens/TaskList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+import todayImage from '../../assets/imgs/today.jpg'
+
+export default class TaskList extends Component {
+    render() {
+        return <View>
+            <Text>TaskList</Text>
+        </View>
+    }
+}
